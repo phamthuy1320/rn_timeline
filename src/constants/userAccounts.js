653f6e0e25d11030924242a13ddeac9cd276ff33
phamthuy1320 <48676769+phamthuy1320@users.noreply.github.com>
2020-08-10 22:22:53 +0700

    [10.08.20]

---
+++ src/constants/userAccounts.js
@@ -0,0 +1,8 @@
+const userAccount = [
+    {'email':'thuy','password':'12345678'},
+    {'email':'nga','password':'12345678'},
+    {'email':'linh','password':'12345678'}
+
+];
+
+export default userAccount;
