ef7ab3728437b7bd4401dcd1f0aa714ddb62c388
phamthuy1320 <48676769+phamthuy1320@users.noreply.github.com>
2020-08-13 06:58:21 +0700

    Add files via upload
    [App->Navigation file]

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 /**
  * @format
  */
-
+//import {document} from 'react-script';
 import {AppRegistry} from 'react-native';
 import App from './App';
-import LoginScreen from './src/screens/LoginScreen';
-
+//import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import ScanScreen from './src/screens/ScanScreen';
+//import CameraAccessScreen from './src/screens/CameraAccessScreen';
+//import AppNavigation from './src/navigations/AppNavigation';
+import AxiosGetData from './src/screens/AxiosGetData';
 
 import {name as appName} from './app.json';
+AppRegistry.registerComponent(appName, () =>App);
 
-
-AppRegistry.registerComponent(appName, () => LoginScreen);
