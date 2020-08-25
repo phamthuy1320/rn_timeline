c8ae7c1bf228e6bde968861b99bdb6c744cd8176
phamthuy1320 <48676769+phamthuy1320@users.noreply.github.com>
2020-08-25 21:44:01 +0700

    [25.08.20] _reupdate

--- src/actions/ContactListActions.js
+++
@@ -1,2 +0,0 @@
-// install redux & react-native-redux
-import * as types from ''
--- src/constants/userAccounts.js
+++
@@ -1,8 +0,0 @@
-const userAccount = [
-    {'email':'thuy@gmail.com','password':'12345678'},
-    {'email':'nga@gmail.com','password':'12345678'},
-    {'email':'linh@gmail.com','password':'12345678'}
-
-];
-
-export default userAccount;
